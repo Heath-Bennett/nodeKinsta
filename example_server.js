dac59d6b3669bc6648f48a8946da9bc11c37d531
Heath Bennett <heathben418@gmail.com>
2023-01-22 13:41:47 -0500

    initialized npm and installed express

--- example_server.js
+++ example_server.js
@@ -1,12 +1,11 @@
-const http = require('http'); //allows us to grab the http package
-const fs = require('fs'); //allows us to connect the server with html pages
-const express = require('express')//allows us to use express in the dode
+const http = require('http');
+const fs = require('fs');
 const homePage = fs.readFileSync('index.html');
 const aboutPage = fs.readFileSync('about.html');
 const contactPage = fs.readFileSync('contact.html');
 const notFoundPage = fs.readFileSync('notFound.html');
-const server = http.createServer( //creates server using the callback function
-    (request, response) =>{
+const server = http.createServer (
+    (request, response) => {
         if (request.url === '/about'){
             response.end(aboutPage);
         }
@@ -20,7 +19,8 @@ const server = http.createServer( //creates server using the callback function
             response.writeHead(404);
             response.end(notFoundPage);
         }
-    });
 
-server.listen(8080)//Starts the server on the specified port.  You can use any port but 1.
-console.log("Listening on port 8080");
+});
+
+server.listen(8080);
+console.log('Server Running on http://localhost:8080');
